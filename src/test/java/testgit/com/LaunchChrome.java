5522ff41610b916598769df827e717849c0d7575
blithe <715478706@qq.com>
2019-01-11 16:58:01 +0800

    第四次

---
+++ src/test/java/testgit/com/LaunchChrome.java
@@ -0,0 +1,34 @@
+package testgit.com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LaunchChrome {
+
+	
+	@Test
+	public void searchBaidu(){
+		System.out.println("准备开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+	@Test
+	public void searchBaidu1(){
+		System.out.println("准备开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+	@Test
+	public void searchBaidu2(){
+		System.out.println("准备开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+	@Test
+	public void searchBaidu3(){
+		System.out.println("准备开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+
+}
