defd718261f7cef055d1d89fa68ccd4499079a1e
blithe <715478706@qq.com>
2019-01-21 17:57:02 +0800

    beforeclass<beforesuite

--- src/test/java/testgit/com/TestngTest.java
+++ src/test/java/testgit/com/TestngTest.java
@@ -1,20 +1,22 @@
 package testgit.com;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class TestngTest {
 
 	
-	@Test
-	public void searchBaidu(){
-		System.out.println("准备开始");
+	@BeforeClass
+	public void searchBaidu1(){
+		System.out.println("@BeforeClass开始");
 		Assert.assertNotEquals(1,2);
 		
 	}
-	@Test
-	public void searchBaidu1(){
-		System.out.println("准备开始");
+	@BeforeSuite
+	public void searchBaidu(){
+		System.out.println("@BeforeSuite开始");
 		Assert.assertNotEquals(1,2);
 		
 	}
@@ -27,7 +29,7 @@ public class TestngTest {
 	@Test
 	public void searchBaidu3(){
 		System.out.println("准备开始");
-		Assert.assertEquals(1,2,"我现在变成true");
+		Assert.assertEquals(2,2,"我现在变成true");
 		
 	}
 
