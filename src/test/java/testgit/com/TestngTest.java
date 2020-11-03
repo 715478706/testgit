7f859bb40ecfa9f4e3a3c776f8217746d824987d
blithe <qq@blithe.com>
2020-11-03 14:41:12 +0800

    加了几个testng的fangfa

--- src/test/java/testgit/com/TestngTest.java
+++ src/test/java/testgit/com/TestngTest.java
@@ -2,40 +2,53 @@ package testgit.com;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestngTest {
 
-	
-	@BeforeClass
+	@BeforeSuite
+	public void searchBaidu5(){
+		System.out.println("@BeforeSuite开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+	@BeforeTest
 	public void searchBaidu1(){
+		System.out.println("@BeforeTest开始");
+		Assert.assertNotEquals(1,2);
+		
+	}
+	@BeforeClass
+	public void searchBaidu(){
 		System.out.println("@BeforeClass开始");
 		Assert.assertNotEquals(1,2);
 		
 	}
-	@BeforeSuite
-	public void searchBaidu(){
-		System.out.println("@BeforeSuite开始");
+	@BeforeMethod
+	public void searchBaidu6(){
+		System.out.println("@BeforeMethod开始");
 		Assert.assertNotEquals(1,2);
 		
 	}
 	@Test
 	public void searchBaidu2(){
-		System.out.println("准备开始");
+		System.out.println("准备开始searchBaidu2");
 		Assert.assertNotEquals(1,2);
 		
 	}
 
 	@Test
 	public void searchBaidu3(){
-		System.out.println("准备开始");
+		System.out.println("准备开始searchBaidu3");
 		Assert.assertEquals(2,2,"我现在变成true");
 
 	}
 	@Test
 	public void searchBaidu4(){
-		System.out.println("准备开始");
+		System.out.println("准备开始searchBaidu4");
 		Assert.assertEquals(2,2,"我现在变成true");
 		
 	}
